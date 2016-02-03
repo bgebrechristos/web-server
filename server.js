27bff2dae501d495ed1dac3611ea175ed67a21dd
Brook Gebrechristos <brookzer@gmail.com>
2016-02-02 18:16:44 -0800

    Remove comments

--- server.js
+++ server.js
@@ -13,21 +13,14 @@ var middleware = {
 	} 
 };
 
-//app.use(middleware.requireAuthentication);
-//this will get called for every page requested and every route hit
-//will be called for index and about
-
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function(req,res) {
-	res.send("About US");
+	res.send("About Us!");
 });
-//when you want to add a route level middleware, use it on the second argument
+
 
 app.use(express.static(__dirname + "/public"));
-
-//static server let you access anything in that folder
-//when viewing a page on browser, you usually doing a get request
 
 app.listen(port, function() {
 	console.log("Server started at port "+ port);
